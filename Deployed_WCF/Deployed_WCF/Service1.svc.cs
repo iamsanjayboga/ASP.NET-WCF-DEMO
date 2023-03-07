4f13faddfe199fe8047f769915a888408de7ae36
iamsanjayboga <iamsanjayboga@gmail.com>
2023-03-07 17:49:51 -0600

    WCF Implementation using ASP.NET

---
+++ Deployed_WCF/Deployed_WCF/Service1.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Deployed_WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int CalulateDays(int day, int month, int year)
+        {
+            DateTime dt = new DateTime(year, month, day);
+            int datetodays = DateTime.Now.Subtract(dt).Days;
+            return datetodays;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetInputFromUser(string userinput)
+        {
+            string modifyuserinput = "WCF Passed Input -- " + userinput;
+            return modifyuserinput;
+        }
+    }
+}
